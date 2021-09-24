b0bf977d4a2e41c5d2ad6f1f7386d91bd1619bcf
Gene4kaS <jannasavchenko@gmail.com>
2021-09-24 21:29:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ const galleryItems = [
         .map(({ preview, original, description }) => {
       return 
       `<li class="gallery__item">
-      <a class="gallery__link" href="">
+      <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
           src="${preview}"
